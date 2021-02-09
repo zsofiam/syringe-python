fecc7ca33c0f5bb5e39dd6d307ce11861de15841
matyasi.zsofia <zsofia.matyasi@gmail.com>
2021-02-09 15:31:33 +0100

    solve task

--- data_manager.py
+++ data_manager.py
@@ -13,5 +13,12 @@ def get_comments(cursor):
 
 @database_common.connection_handler
 def create_comment(cursor, name, text):
-    query = f"INSERT INTO comment (name, text) VALUES ('{name}', '{text}')"
+
+    #query = f"INSERT INTO comment (name, text) VALUES ('{name}', '{text}')"
+    query = """
+           INSERT INTO COMMENT (name, text) 
+           VALUES (name = %(username)s, text = %(text)s)
+        """, {
+        'username': name, 'text': text
+    }
     cursor.execute(query)
